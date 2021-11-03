fb144052277989e699631f28e7435487385c73f9
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2021-11-03 19:37:37 -0400

    HHHHH

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/build'));
     app.get('*',(req,res) =>
     {
         res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
