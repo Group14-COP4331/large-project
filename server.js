d6b5584df9b099d69a5d3ddad5339e61b9c81c6c
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2021-12-07 22:31:35 -0500

    last try

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(favicon(path.join(__dirname,'client','public','favicon.ico')));
 app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
+    app.use(favicon(path.join(__dirname,'client','build','favicon.ico')))
     app.get('*',(req,res) =>
     {
         res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
