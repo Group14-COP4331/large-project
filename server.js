bcabd7ac6ec3cda2a8e0ac0d1168254f1955903c
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2021-12-07 22:22:14 -0500

    favicon

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(favicon(path.join(__dirname,'public','favicon.ico')));
+app.use(favicon(path.join(__dirname,'client','public','favicon.ico')));
 app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
